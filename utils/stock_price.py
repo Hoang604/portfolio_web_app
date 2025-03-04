232bcbb0840dde47675920d6fcee7b0bc11d49c4
Hoang604 <dinhviethoang604@gmail.com>
2025-03-04 16:07:53 +0700

    delete stock_prices cache

--- utils/stock_price.py
+++ utils/stock_price.py
@@ -1,11 +1,6 @@
-stock_prices = {}
-
 def get_current_stock_price(stock_code, db):
-    if stock_code in stock_prices:
-        return stock_prices[stock_code]
     cursor = db.cursor()
     cursor.execute(f"SELECT price FROM stock_prices where stock_code = '{stock_code}' ORDER BY date DESC LIMIT 1")
     stock_price = cursor.fetchone()
-    stock_price = float(stock_price[0]) 
-    stock_prices[stock_code] = stock_price
+    stock_price = float(stock_price[0]) * 1000
     return stock_price
